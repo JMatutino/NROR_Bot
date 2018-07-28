ef08207a7e9cfff9c94956eb78de0a1d9c88e82f
JMatutino <jacobmatutino@gmail.com>
2018-07-28 12:07:04 -1000

    Added gw2 api

--- nror-bot.js
+++ nror-bot.js
@@ -1,23 +1,30 @@
-let Discord = require("discord.js");
+let Discord = require('discord.js');
 let bot = new Discord.Client();
 
-let auth = require("./auth.json"); //Create a file called "auth.json" in same dir
+let GuildInfo = require('./nror-info.js');
+const guildInfo = new GuildInfo();
 
-let prefix = "~";
+let auth = require('./auth.json'); //Create a file called "auth.json" in same dir
+
+let prefix = '~';
 
 
-bot.on("message", msg => {
+bot.on('message', msg => {
   if(!msg.content.startsWith(prefix) || msg.author.bot) {
     return;
   }
 
-  if(msg.content.startsWith(prefix+"hello")) {
-    msg.reply("Hello to you too!");
+  if(msg.content.startsWith(prefix + 'hello')) {
+    msg.reply('Hello to you too!');
+  }
+
+  if(msg.content.startsWith(prefix + 'stats')) {
+    msg.channel.send(guildInfo.getAllGuildInfo());
   }
 });
 
-bot.on("ready", msg => {
-  console.log("Bot ready");
+bot.on('ready', msg => {
+  console.log('Bot ready');
 });
 
 bot.login(auth.token);
